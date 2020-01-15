6118da189791fc0ae6e47f7f03ee8b40cf8fae44
arrbxr <arr.bxr@gmail.com>
2020-01-15 15:13:51 +0530

    Updates

---
+++ precache-manifest.e635acf2005ceee8176e653229873da2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2cf5aa9407c082ed9165a383c308ce",
+    "url": "/pic/index.html"
+  },
+  {
+    "revision": "d944b720a5236866358b",
+    "url": "/pic/static/css/main.5c2f294a.chunk.css"
+  },
+  {
+    "revision": "1dd2b1d46143c9fdb697",
+    "url": "/pic/static/js/2.d45f133c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pic/static/js/2.d45f133c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d944b720a5236866358b",
+    "url": "/pic/static/js/main.630ba0d8.chunk.js"
+  },
+  {
+    "revision": "c36a06a5b077cf55c400",
+    "url": "/pic/static/js/runtime-main.a89f12ef.js"
+  }
+]);
